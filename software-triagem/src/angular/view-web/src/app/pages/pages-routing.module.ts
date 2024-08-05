565a831d0f27a7a95e4a92b7767dfd496614403c
valdeir cesario <135670289+valdeircesario@users.noreply.github.com>
2024-08-05 20:00:15 -0300

    ajustes paginas

--- software-triagem/src/angular/view-web/src/app/pages/pages-routing.module.ts
+++ software-triagem/src/angular/view-web/src/app/pages/pages-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AppHomeComponent} from "./app-home/app-home.component";
 import {AppUsersComponent} from "./app-users/app-users.component";
+import { AppCadastrarComponent } from './app-cadastrar/app-cadastrar.component';
+import {AppTriagemComponent} from "./app-triagem/app-triagem.component";
 
 const routes: Routes = [
   {path: 'home', component: AppHomeComponent},
   {path: 'users', component: AppUsersComponent},
+  { path: 'cadastrar', component: AppCadastrarComponent },
+  { path: 'triagem', component: AppTriagemComponent },
 ];
 
 @NgModule({
