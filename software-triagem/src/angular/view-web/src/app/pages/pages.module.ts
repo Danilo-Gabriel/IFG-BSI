e1596b35118ae6ccc4df333cc06f1e426490a69d
valdeir cesario <135670289+valdeircesario@users.noreply.github.com>
2024-08-05 20:00:15 -0300

    ajustes paginas

--- software-triagem/src/angular/view-web/src/app/pages/pages.module.ts
+++ software-triagem/src/angular/view-web/src/app/pages/pages.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import {AppHomeComponent} from "./app-home/app-home.component";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppUsersComponent } from './app-users/app-users.component';
+import { AppCadastrarComponent } from './app-cadastrar/app-cadastrar.component';
+import { AppTriagemComponent } from './app-triagem/app-triagem.component';
 
 
 @NgModule({
   declarations: [
 
+
+    AppCadastrarComponent,
+         AppTriagemComponent
   ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    PagesRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        PagesRoutingModule,
+        FormsModule
+    ]
 })
 export class PagesModule { }
