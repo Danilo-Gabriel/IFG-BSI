0854fdf949add205bc18d59cf90222066bd3e567
valdeir cesario <135670289+valdeircesario@users.noreply.github.com>
2024-08-16 18:19:45 -0300

    atualizacoes

--- software-triagem/src/angular/view-web/src/app/pages/app-cadastrar/app-cadastrar.component.ts
+++ software-triagem/src/angular/view-web/src/app/pages/app-cadastrar/app-cadastrar.component.ts
@@ -1,54 +1,51 @@
 import { Component } from '@angular/core';
 import { CadastrarService } from './service/cadastro.service';
 import { CadastroDto } from '../../../model/cadastrar/cadastrar-dto';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-app-cadastrar',
-    templateUrl: './app-cadastrar.component.html',
-    styleUrls: ['./app-cadastrar.component.scss']
+  selector: 'app-app-cadastrar',
+  templateUrl: './app-cadastrar.component.html',
+  styleUrls: ['./app-cadastrar.component.scss']
 })
 
 export class AppCadastrarComponent {
 
+  formCadastro: FormGroup;
 
-    formCadastro: FormGroup;
+  constructor(private cadastrarService: CadastrarService, private fb: FormBuilder) {
+    this.formCadastro = this.fb.group({
+      fullName: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]], // Adicione validação de email
+      password: ['', [Validators.required, Validators.minLength(8)]], // Adicione validação de senha
+      confirmPassword: ['', Validators.required]
+    });
+  }
 
-    constructor(private cadastrarService: CadastrarService, private fb: FormBuilder) {
-        this.formCadastro = this.fb.group({
-            fullName: ['', Validators.required],
-            phone: ['', Validators.required],
-            address: ['', Validators.required],
-            birthDate: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]], // Adicione validação de email
-            password: ['', [Validators.required, Validators.minLength(8)]], // Adicione validação de senha
-            confirmPassword: ['', Validators.required]
-        });
+  onSubmit() {
+    // Validar as senhas
+    if (this.formCadastro.get('password')!.value !== this.formCadastro.get('confirmPassword')!.value) {
+      alert('As senhas não coincidem. Por favor, tente novamente.');
+      return;
     }
 
-    onSubmit() {
-        // Validar as senhas
-        if (this.formCadastro.get('password')!.value !== this.formCadastro.get('confirmPassword')!.value) {
-            alert('As senhas não coincidem. Por favor, tente novamente.');
-            return;
-        }
+    // Criar um objeto com os dados do formulário
+    const userData: CadastroDto = this.formCadastro.value;
 
-        // Criar um objeto com os dados do formulário
-        const userData: CadastroDto = this.formCadastro.value;
-
-        // Chamar o serviço para enviar os dados para o backend
-        this.cadastrarService.efetuarCadastro(userData).subscribe(
-            response => {
-                console.log('Resposta do servidor:', response);
-                alert('Cadastro realizado com sucesso!');
-                this.formCadastro.reset(); // Limpar o formulário após o sucesso
-            },
-            error => {
-                console.error('Erro ao cadastrar usuário:', error);
-                alert('Ocorreu um erro ao realizar o cadastro. Por favor, tente novamente.');
-            }
-        );
-    }
-
-
+    // Chamar o serviço para enviar os dados para o backend
+    this.cadastrarService.efetuarCadastro(userData).subscribe({
+      next: (response) => {
+        console.log('Resposta do servidor:', response);
+        alert('Cadastro realizado com sucesso!');
+        this.formCadastro.reset(); // Limpar o formulário após o sucesso
+      },
+      error: (error) => {
+        console.error('Erro ao cadastrar usuário:', error);
+        alert('Ocorreu um erro ao realizar o cadastro. Por favor, tente novamente.');
+      }
+    });
+  }
 }
